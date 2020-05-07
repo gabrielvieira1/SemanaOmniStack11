4dd82ed475857d17d226aaf67fa63f676f6e9b48
gabrielvieira1 <gabrielvieira1@protonmail.com>
2020-05-07 19:24:32 -0300

    Atualização controler

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -3,9 +3,7 @@ const connection = require('../database/connection');
 module.exports = {
     async index(request, response){
         const { page = 1 } = request.query;
-
         const [count] = await connection('incidents').count();
-
         const incidents = await connection('incidents')
             .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
             .limit(5)
